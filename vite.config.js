57e2a8f33b8dc9aee2cf97692af2f6af6b9b9e87
HamedAhmedHamed <hamedahmedgamed@gmail.com>
2024-09-02 13:25:12 +0300

    add ssl certs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: "./private.key",
+      cert: "./certificate.crt"
+    }
+  },
   plugins: [react()],
 })
